27f7986e788b8274d36b06c6beea9d8a5a0f6ccd
florida0723 <772063154@qq.com>
2018-04-10 10:09:36 +0800

    success

--- index.js
+++ index.js
@@ -8,49 +8,44 @@ var reqAnimationFrame = (function () {
 
 (function(){
   var defaults = {
-    'translate': {    // 定义 2D 转换。
-		'x': 0,
-		'y': 0
-	},
+    'translateX': 0,
+    'translateY': 0,
 	'scale': 1,    // 定义 2D 缩放转换。
 	'angle': 0,    // 定义 2D 旋转，在参数 中规定角度。deg
     'ticking': false
   }
-  
+
   function touch(id, options){
-    
+
     options = options || {};
-	  
+
     this.el = document.querySelector(id);
-    
-    this.opts = Hammer.extend(defaults, options);
-	
-	this.old = Hammer.extend({}, this.opts);	// 缓存原始数据
-	
+
+    this.opts = Hammer.extend({}, Hammer.extend(defaults, options));
+
+	this.old = Hammer.extend({}, this.opts, true);	// 缓存原始数据
+
     this.init();
+
+    return this;
   }
-  
+
   touch.prototype.init = function(){
     var _this = this;
-    
+
     var handler = new Hammer(this.el);
     handler.get('pinch').set({ enable: true });
     handler.get('rotate').set({ enable: true });
-    
+
     this.elementUpdate();
-    
+
 	handler.on("panstart panmove", function(ev){
-		console.log(_this.opts.translate);
-      _this.opts.translate.x += parseInt(ev.deltaX);
-      _this.opts.translate.y += parseInt(ev.deltaY);
-      
-      _this.opts.translate = {
-        x: parseInt(_this.old.translate.x + ev.deltaX),
-        y: parseInt(_this.old.translate.y + ev.deltaY)
-      };
+      _this.opts.translateX = parseInt(_this.old.translateX + ev.deltaX);
+      _this.opts.translateY = parseInt(_this.old.translateY + ev.deltaY);
+
       _this.elementUpdate();
     });
-    
+
 	this.initAngle = 0;
     handler.on("rotatestart rotatemove", function(ev){
 	  if(ev.type == 'rotatestart') {
@@ -59,28 +54,28 @@ var reqAnimationFrame = (function () {
       _this.opts.angle = _this.initAngle + parseFloat(ev.rotation);
       _this.elementUpdate();
 	});
-    
+
 	this.initScale = 1;
 	handler.on("pinchstart pinchmove", function(ev){
 	  if(ev.type == 'pinchstart') {
-        _thisinitScale = _this.opts.scale || 1;
+        _this.initScale = _this.opts.scale || 1;
       }
-
       _this.opts.scale = _this.initScale * parseFloat(ev.scale);
-
       _this.elementUpdate();
 	});
-	
+
 	handler.on("hammer.input", function(ev) {
       if(ev.isFinal) {
-        _this.old.translate.x = _this.opts.translate.x;
-        _this.old.translate.y = _this.opts.translate.y;
-        
+        _this.old.translateX = _this.opts.translateX;
+        _this.old.translateY = _this.opts.translateY;
+        _this.old.scale = _this.opts.scale;
+        _this.old.angle = _this.opts.angle;
+
         _this.updateElementTransform();
       }
-    });  
+    });
   };
-  
+
   touch.prototype.elementUpdate = function(){
     var _this = this;
     if(!this.opts.ticking) {
@@ -88,25 +83,25 @@ var reqAnimationFrame = (function () {
       this.opts.ticking = true;
     }
   };
-  
+
   touch.prototype.updateElementTransform = function(){
     var _this = this;
-	
+
     var value = [
-      'translate(' + this.opts.translate.x + 'px, ' + this.opts.translate.y + 'px)',
+      'translate(' + this.opts.translateX + 'px, ' + this.opts.translateY + 'px)',
       'scale(' + this.opts.scale + ', ' + this.opts.scale + ')',
       'rotate('+ this.opts.angle + 'deg)'
     ];
 
     value = value.join(" ");
-    
+
     _this.el.textContent = value;
     _this.el.style.webkitTransform = value;
     _this.el.style.mozTransform = value;
     _this.el.style.transform = value;
-    
+
     _this.opts.ticking = false;
   };
-  
+
   window['touch'] = touch;
 })()
